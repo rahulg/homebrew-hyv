d5eca7ffd749c61d07f263eb70f6f57821dfc001
Rahul AG <r@hul.ag>
2015-07-10 12:28:53 +0800

    hyv is now tagged.

--- hyv.rb
+++ hyv.rb
@@ -1,6 +1,10 @@
 class Hyv < Formula
   desc "CLI tool to easily run xhyve VMs"
+  url "https://github.com/rahulg/hyv/archive/0.1.0.tar.gz"
+  sha256 "31bc129c2a67c1f0029377ab4ce5c9c96844c7aabf7535739c77a1372c785541"
   head "https://github.com/rahulg/hyv.git"
+
+  depends_on :macos => :yosemite
 
   def install
     bin.install "hyv"
